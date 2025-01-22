83ed1b1e91c0e66ab6cc8f8c1e01ef2f018a053e
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2025-01-22 13:14:24 +0530

    debug issue

--- src/components/MenuDisplay.js
+++ src/components/MenuDisplay.js
@@ -2,23 +2,24 @@ import React, { useState } from "react";
 import Dishes from "./Items";
 
 
-const MenuDisplay = ({category}) =>{
+const MenuDisplay = ({ category }) => {
     let [items, setItems] = useState(Dishes)
     console.log(category)
     return (
         <>
             <div id="main" className="menu-div">
                 {
-                    items.map((item) =>{
-                        (item.category === category || category === "all") &&
-                        (
+                    items.map(item =>(
+                        (item.category === category || category === "all") && (
                             <div data-test-id={`menu-item-${category}`} className="item-card">
                                 <h1>{item.title}</h1>
                                 <p>{item.price}</p>
                                 <p>{item.desc}</p>
+                                <img src={item.img} />
                             </div>
                         )
-                    })
+                    )
+                    )
                 }
             </div>
         </>
